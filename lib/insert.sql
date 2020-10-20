2be87433219b05d749da9acc84d047fbb5ff702b
Rocio Batres <jonasgirl21@gmail.com>
2020-10-20 01:30:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,7 +5,7 @@ INSERT INTO authors (id, name) VALUES (1, "Togashi"), (2, "Takeushi");
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Volume 1", 1999, 1), (2, "Volume 32", 2003, 1), (3, "Volume 24", 2000, 1), (4, "Volume 1", 1998, 2), (5, "Volume 18", 1999, 2), (6, "Volume 48", 2001, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Sailor Moon", "In the power of the moon, I will punish you!", "Sailor Guardian", 2), (2, "Tuxedo Mask", "*throws a rose*", 2), (3, "Sailor Mars", "I'm Sailor Mars of the red heel!", "Sailor Guardian", 2), (4, "Chibiusa", "I am a graceful lady", "Pesky child", 2);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Sailor Moon", "In the power of the moon, I will punish you!", "Sailor Guardian", 2), (2, "Tuxedo Mask", "*throws a rose*", "typical hero", 2), (3, "Sailor Mars", "I'm Sailor Mars of the red heel!", "Sailor Guardian", 2), (4, "Chibiusa", "I am a graceful lady", "Pesky child", 2);
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Gon Freeces", "In my next life, I want to be me, and meet you again", "Hunter", 1), (6, "Killua Zoldyck", "Not killing people is hard", "Hunter", 1), (7, "Hisoka Marrow", "Bungee gum has the properties of both rubber and gum", "Hunter", 1), (8, "Ging", "Enjoy the little things", "Deadbeat dad", 1);
 
